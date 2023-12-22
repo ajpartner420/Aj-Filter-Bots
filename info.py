75fd56a40837dd55eb97d7046b70b2b2dce40328
ajpartner420 <115463583+ajpartner420@users.noreply.github.com>
2023-12-22 20:28:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Aj_movieBook:jitu3240@cluster0.hdimszu.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "aj_moviebook")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
