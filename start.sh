617ba51715c3709319efb3414a16270f9cba2a26
ajpartner420 <115463583+ajpartner420@users.noreply.github.com>
2023-12-22 08:08:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @Aj_MovieBook
 
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/ajpartner420/AJ-FILTER-BOT /AJ-FILTER-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /AJ-FILTER-BOT 
 fi
-cd /VJ-FILTER-BOT 
+cd /AJ-FILTER-BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
