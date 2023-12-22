75407916e579b2787ed709cb13a504ed4c6f99ce
ajpartner420 <115463583+ajpartner420@users.noreply.github.com>
2023-12-22 08:45:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ajpartner420/AJ-FILTER-BOT /AJ-FILTER-BOT 
+  git clone https://github.com/ajpartner420/Aj-Filter-Bots /Aj-Filter-Bots 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Aj-Filter-Bots 
 fi
-cd /AJ-FILTER-BOT 
+cd /Aj-Filter-Bots 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
